6ced6a1e70938dfdbe1f49a34242fc2fe27a5d6a
Kacper Kwiatkowski <kacper18k@gmail.com>
2022-11-22 12:48:58 +0100

    Banner amended, animation added.

--- apps/content/src/app/app.module.ts
+++ apps/content/src/app/app.module.ts
@@ -3,24 +3,26 @@
  * compilation, allowing it to be included in the built bundle. This is required
  * for the Module Federation Plugin to expose the Module correctly.
  * */
-import { ContentEntryModule } from './remote-entry/entry.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { BannerComponent } from './components/banner/banner.component';
+import { CategorySelectorComponent } from './components/category-selector/category-selector.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    exports: [
-        AppComponent
-    ]
+  declarations: [AppComponent, BannerComponent, CategorySelectorComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [AppComponent],
 })
 export class AppModule {}
