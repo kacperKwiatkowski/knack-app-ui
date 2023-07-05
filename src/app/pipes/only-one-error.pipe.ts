a98a27b24bd6b0ba54a4a7ed0965086c65c9fe1a
kacperKwiatkowski <kacper18k@gmail.com>
2023-07-05 20:16:54 +0200

    Pipe and directive added.

---
+++ src/app/pipes/only-one-error.pipe.ts
@@ -0,0 +1,25 @@
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+  name: 'onlyOneError'
+})
+export class Only0neErrorPipe implements PipeTransform{
+
+  transform(allErrors: any, errorsPriority: string[]): any {
+
+    if (!allErrors){
+      return null;
+    }
+
+    const onlyOneError: any = {};
+
+    for (let error of errorsPriority){
+      if(allErrors[error]){
+        onlyOneError[error] = allErrors[error]
+        break
+      }
+    }
+
+    return onlyOneError;
+  }
+}
