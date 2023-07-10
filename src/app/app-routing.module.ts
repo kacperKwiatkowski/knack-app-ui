bc23b09e7eaa9c7772b8989a3e33e012b918d610
kacperKwiatkowski <kacper18k@gmail.com>
2023-07-10 16:56:56 +0200

    Custom loading strategy implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {LoginSiteComponent} from "./sites/login-site/login-site.component";
 import {HomeSiteComponent} from "./sites/home-site/home-site.component";
 import {RegisterFormComponent} from "./components/register-form/register-form.component";
 import {PageNotFoundSiteComponent} from "./sites/page-not-found-site/page-not-found-site.component";
+import {CustomPreloadingStrategy} from "./services/custom-preloading.strategy";
 
 const routes: Routes = [
   {
     path: "",
     redirectTo: "home",
     pathMatch: "full"
-  },{
+  }, {
     path: "home",
     component: HomeSiteComponent
   }, {
@@ -21,7 +22,10 @@ const routes: Routes = [
     component: RegisterFormComponent
   }, {
     path: "basket",
-    loadChildren: () => import('./modules/basket/basket.module').then(m => m.BasketModule)
+    loadChildren: () => import('./modules/basket/basket.module').then(m => m.BasketModule),
+    data: {
+      preload: false
+    }
   }, {
     path: "**",
     component: PageNotFoundSiteComponent
@@ -29,8 +33,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: CustomPreloadingStrategy
+  })],
+  exports: [RouterModule],
+  providers: [CustomPreloadingStrategy]
 })
 export class AppRoutingModule {
 }
